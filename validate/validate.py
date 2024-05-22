b0f921dfb097a47696ea1fc91dd17439190e08d6
Franciszek Zdobylak <fzdobylak@antmicro.com>
2024-05-22 10:31:58 +0200

    [#59733] validate: Check for vendor icons

--- validate/validate.py
+++ validate/validate.py
@@ -19,24 +19,31 @@ def validate_schema(validate_dict, schema):
         return False
 
 
-def validate_vendor_logos(vendors, logos_dir):
+def validate_vendor_images(vendors, logos_dir, icons_dir):
     missing_logos = []
+    missing_icons = []
+
     for v_name, v_data in vendors.items():
         logo = v_data.get("logo", True)
         if not logo:
             continue
 
         logo_path = logos_dir / f"{v_name}.svg"
+        icon_path = icons_dir / f"{v_name}.svg"
         if not logo_path.exists():
             missing_logos.append(logo_path)
 
-    return missing_logos
+        if not icon_path.exists():
+            missing_icons.append(icon_path)
+
+    return missing_logos, missing_icons
 
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("vendor_list")
-    parser.add_argument("-l", "--logos", type=Path, default=Path("logos/"))
+    parser.add_argument("-l", "--logos", type=Path, default=Path("vendor-logos/"))
+    parser.add_argument("-i", "--icons", type=Path, default=Path("vendor-icons/"))
     parser.add_argument("-s", "--schema", type=Path, default=Path("validate/vendor-list-schema.yaml"))
     args = parser.parse_args()
 
@@ -50,10 +57,21 @@ def main():
         print(f"Format of {args.vendor_list} is invalid!", file=sys.stderr)
         sys.exit(1)
 
-    if missing := validate_vendor_logos(vendors, args.logos):
-        print(f"There are {len(missing)} missing logos:")
-        print("\n".join(f" - {p}" for p in missing))
-        sys.exit(1)
+    return_code = 0
+
+    missing_logos, missing_icons = validate_vendor_images(vendors, args.logos, args.icons)
+
+    if missing_logos:
+        print(f"There are {len(missing_logos)} missing logos:")
+        print("\n".join(f" - {p}" for p in missing_logos))
+        return_code = 1
+
+    if missing_icons:
+        print(f"There are {len(missing_icons)} missing icons:")
+        print("\n".join(f" - {p}" for p in missing_icons))
+        return_code = 1
+
+    sys.exit(return_code)
 
 
 if __name__ == "__main__":
